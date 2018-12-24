0af6b6d1fdfd781b12ad04578821f971e24c50c3
geops22 <geops22@gmail.com>
2018-12-24 14:45:29 -0600

    v0.5

--- cc.sh
+++ cc.sh
@@ -10,15 +10,8 @@ main () {
   i=1
   sp="/-\|"
 
-  if [[ ( "$1" != "install" ) && ( "$1" != "system" ) ]]; then
-
-    if [ -f $data/.env ]; then
-      network="$(cat $data/.env | grep DATA | awk -F"_" '{print $4}')"
-    else
-      echo -e "\nCore not properly installed. Please install first.\n"
-      exit 1
-    fi
-
+  if [ -f $data/.env ]; then
+    network="$(cat $data/.env | grep DATA | awk -F"_" '{print $4}')"
   fi
 
   if [[ ( "$1" == "install" ) && ( "$2" = "mainnet" || "$2" = "devnet" ) && ( -z "$3" ) ]]; then
@@ -83,6 +76,11 @@ main () {
 
   elif [[ ( "$1" == "remove" ) && ( -z "$2" ) ]]; then
 
+    if [[ ! -d $data && ! -d $core ]]; then
+      echo "\nCore not installed.\n"
+      exit 1
+    fi
+
     system
 
     remove $network &
@@ -97,11 +95,21 @@ main () {
 
   elif [[ ( "$1" == "start" ) && ( "$2" = "relay" || "$2" = "forger" || "$2" = "all" ) && ( -z "$3" ) ]]; then
 
+    if [[ ! -d $data || ! -d $core ]]; then
+      echo "\nCore not installed. Please install first.\n"
+      exit 1
+    fi
+
     start $2 $network
 
     echo -e "\nAll Done!\n"
 
   elif [[ ( "$1" == "stop" ) && ( "$2" = "relay" || "$2" = "forger" || "$2" = "all" ) && ( -z "$3" ) ]]; then
+
+    if [[ ! -d $data || ! -d $core ]]; then
+      echo "\nCore not installed. Please install first.\n"
+      exit 1
+    fi
 
     stop "$2"
 
