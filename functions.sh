43d8c6498f31ca5b1feb2c611b8de15c6d94f66b
geops22 <geops22@gmail.com>
2018-12-26 15:55:26 -0600

    v0.5

--- functions.sh
+++ functions.sh
@@ -220,6 +220,7 @@ remove () {
 
   pm2 delete ${name}-core-forger > /dev/null 2>&1
   pm2 delete ${name}-core-relay > /dev/null 2>&1
+  pm2 save > /dev/null 2>&1
   rm -rf $core && rm -rf $data > /dev/null 2>&1
   sudo rm -rf $HOME/.config > /dev/null 2>&1
   dropdb ${name}_$1 > /dev/null 2>&1
