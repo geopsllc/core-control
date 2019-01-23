4be26dfa14ba458e546b0a9ee36d91ee9de06c67
Georgi Stoyanov <33185290+geopsllc@users.noreply.github.com>
2019-01-23 16:34:46 -0600

    devnet

--- misc.sh
+++ misc.sh
@@ -8,7 +8,3 @@ als="$(cat $HOME/.bashrc | grep ccontrol)"
 if [ -z "$als" ]; then
   echo "alias ccontrol=$PWD/ccontrol.sh" >> $HOME/.bashrc
 fi
-
-if [ -f $data/.env ]; then
-  network="$(cat $data/devnet/.env | grep DATA | awk -F"_" '{print $4}')"
-fi
