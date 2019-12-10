1d00d1796c4caa71fd509878cf527dd4d4364710
Georgi Stoyanov <33185290+geopsllc@users.noreply.github.com>
2019-12-10 09:44:53 -0600

    2.6

--- functions.sh
+++ functions.sh
@@ -439,32 +439,26 @@ secret () {
 
 snapshot () {
 
+  local fstatus=$(pm2status "${name}-forger" | awk '{print $4}')
+  local rstatus=$(pm2status "${name}-relay" | awk '{print $4}')
+  stop all > /dev/null 2>&1
+
   if [ "$1" = "restore" ]; then
-
-    local fstatus=$(pm2status "${name}-forger" | awk '{print $4}')
-    local rstatus=$(pm2status "${name}-relay" | awk '{print $4}')
-
-    stop all > /dev/null 2>&1
-
     dropdb ${name}_$network > /dev/null 2>&1
     createdb ${name}_$network > /dev/null 2>&1
-
     $core/core/bin/run snapshot:restore --network $network --token $name
-
-    if [ "$rstatus" = "online" ]; then
-      start relay > /dev/null 2>&1
-    fi
-
-    if [ "$fstatus" = "online" ]; then
-      start forger > /dev/null 2>&1
-    fi
-
   else
-
     $core/core/bin/run snapshot:dump --network $network --token $name
-
   fi
 
+  if [ "$rstatus" = "online" ]; then
+    start relay > /dev/null 2>&1
+  fi
+
+  if [ "$fstatus" = "online" ]; then
+    start forger > /dev/null 2>&1
+  fi
+    
 }
 
 selfremove () {
